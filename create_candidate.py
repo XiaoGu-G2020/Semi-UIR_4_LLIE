4a49b6ca39208dbe09cdd21955115cbab7baeda9
XiaoGu <1823355358@qq.com>
2025-03-25 14:48:49 +0800

    Docs：主要修改了requirement.txt，修改了部分模块的版本以运行Semi-UIR框架。

---
+++ create_candidate.py
@@ -0,0 +1,22 @@
+import os
+import torch
+from glob import glob
+from os.path import join
+from torchvision.transforms import transforms
+
+# initialize the reliable bank
+
+# input_dir = 'data/unlabeled/input'
+# result_dir = 'data/unlabeled/candidate'
+input_dir = 'data/unlabeled/input'
+result_dir = 'data/unlabeled/candidate'
+if result_dir not in os.listdir(input_dir):
+    os.makedirs(result_dir)
+input_lists = glob(join(input_dir, '*.*'))
+for gen_path in zip(input_lists):
+    img = torch.zeros((3,256,256))
+    img_name = gen_path[0].split('/')[-1].split('\\')[-1]
+    print(img_name)
+    toPil = transforms.ToPILImage()
+    res = toPil(img).convert('RGB')
+    res.save(os.path.join(result_dir, img_name))
---
+++ version.py
@@ -0,0 +1,9 @@
+import torch
+import mmcv
+import torchvision
+import skimage
+
+print(torch.__version__)
+print(mmcv.__version__)
+print(torchvision.__version__)
+print(skimage.__version__)
